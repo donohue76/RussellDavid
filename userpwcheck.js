5369d32c1cfb55f56f6920c1b55eecd6a28387ee
LEARN <learn@LEARN03.local>
2016-01-25 16:26:32 -0800

    final changes

--- userpwcheck.js
+++ userpwcheck.js
@@ -5,20 +5,23 @@ var password = prompt("Enter Password");
 function goodId(userId) {return !(userId.includes("#") || userId.includes("!") || userId.includes("$")) && userId.length > 5}
 
 /*checks password for number, special character reqs, length and casing reqs.*/
-function goodPassword(password) {return (password.includes("#") || password.includes("!") || password.includes("$")) && !(password.includes("password")) && password.length > 5 && hasNumber(password) && upCase(password) && downCase(password)};
+function goodPassword(password) {return (password.includes("#") || password.includes("!") || password.includes("$")) && !(password.includes("password")) && password.length > 5 && hasNumber(password) && pwCaps(password)};
 
 /* checks to see that userId and password are not the same*/
 function matching(userId, password) {return userId != password}
 
-/*Validates userId and password */
+/*Validates userId and password*/
 function validCred(userId, password) {return goodId(userId) && goodPassword(password) && matching(userId, password)};
 
-/*Prompts user for validation credentials and alerts user is credentials are invalid */
+/*Prompts user for validation credentials and alerts user is credentials are invalid*/
 function getCreds() {var userId = prompt("Enter user ID"); var password = prompt("Enter Password"); alert("Your creds are valid:" + validCred(userId, password));}
 
-/* Checks for at least one upper and one lowercase letter*/
+/*Checks for all uppercase, if it is, it returns false*/
 function downCase(password) { return password !== password.toUpperCase() };
+/*Checks for all lowercase, if it is, it returns false*/
 function upCase(password) { return password !== password.toLowerCase() };
 
 /*Checks for at least one number*/
 function hasNumber(password) {return password.includes("0") || password.includes("1") || password.includes("2") || password.includes("3") || password.includes("4") || password.includes("5") || password.includes("6") || password.includes("7") || password.includes("8") || password.includes("9")};
+
+function pwCaps(password) {return downCase(password) && upCase(password)};
